4f82b0ece6d5ec176288ba67cfd6293b62a9f97b
kaven <zmx729618@163.com>
2017-12-13 10:02:48 +0800

    zxing生成二维码的工具类

---
+++ src/main/java/org/zwc/ArrayDequeTest/MainTest.java
@@ -0,0 +1,36 @@
+package org.zwc.ArrayDequeTest;
+
+import java.util.ArrayDeque;
+
+/**
+ * Created by zhangwenchao on 2017/12/13.
+ */
+public class MainTest {
+
+    public static void main(String[] args) {
+
+        ArrayDeque<Integer> deque = new ArrayDeque<Integer>();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addFirst(3);
+        deque.addFirst(4);
+        deque.addFirst(5);
+
+        for(Integer i : deque){
+            System.out.println(i);
+        }
+        System.out.println("-------------------------------");
+
+        Integer e = deque.removeFirst();
+        System.out.println(e);
+        System.out.println("-------------------------------");
+
+        Integer e1 = deque.peekFirst();
+        System.out.println(e1);
+        System.out.println("-------------------------------");
+
+        for(Integer i : deque){
+            System.out.println(i);
+        }
+    }
+}
